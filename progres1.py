ee5f3bde4a1343c37db205a52dcba61b15dfd9f8
divashafa <divashafa.2020@student.uny.ac.id>
2021-09-30 14:11:29 +0700

    first commit

---
+++ progres1.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+master = tk.Tk()
+master.title("APK LAYANAN RUMAH SAKIT")
+master.geometry("300x300")
+
+tk.Label(master, text="---Selamat Datang Di Pusat Pelayanan Rumah Sakit Tadika Mesra---").grid(row=0, column=0)
+
+tk.mainloop()
